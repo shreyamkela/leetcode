f55ef0f825b1311a60db4162b33e0fd22931acb9
Shreyam <shreyamkela@gmail.com>
2018-09-21 13:47:35 -0700

    Update MoveZeroes.java

--- Java/Top Interview Questions/Easy/MoveZeroes.java
+++ Java/Top Interview Questions/Easy/MoveZeroes.java
@@ -2,9 +2,9 @@ class Solution {
     public void moveZeroes(int[] nums) {
         
         int temp;
-        int zeropos=0;
+        int zeropos = 0;
         for(int i=0; i<nums.length; i++) {
-            if(nums[i]!=0) {
+            if(nums[i] != 0) {
                 temp = nums[i];
                 nums[i] = nums[zeropos];
                 nums[zeropos] = temp;
